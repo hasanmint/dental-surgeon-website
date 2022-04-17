a005af649a790a5f03edd4e752043b89b1b043c6
hasanmint73 <hasanmint73@gmail.com>
2022-04-17 20:54:50 +0600

    Login & Register form form submit

--- src/Pages/Auth/Login/Login.js
+++ src/Pages/Auth/Login/Login.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
     const [
         signInWithEmailAndPassword,
         user,
@@ -12,23 +12,40 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
-    
+    if (user) {
+        navigate('/register')
+    }
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        // console.log(email, password);
+        signInWithEmailAndPassword(email, password)
+    }
+
 
     return (
         <div className='container mt-5 mb-5'>
             <div className="row">
                 <div className="col-md-6 offset-md-3 shadow-none p-3 mb-5 bg-light rounded">
-                    <h2 className="text-center">Login</h2>
-                    <form>
+                    <h1 className="text-center fw-bolder">Login</h1>
+                    <div className='text-dark text-center fw-bolder'>
+                        <p> New Here? <Link to='/register' className='text-success text-decoration-none fw-bold'>Create An Account</Link></p>
+                    </div>
+                    <form onSubmit={handleLogin}>
                         <div className="form-group mb-3">
-                            <input type="text" className='form-control' name="email" id="" placeholder='Enter Email' />
+                            <input type="email" className='form-control' name="email" id="" placeholder='Enter Email' />
                         </div>
 
                         <div className="form-group mb-3">
                             <input type="password" className='form-control' name="password" id="" placeholder='Enter Password' />
                         </div>
-                        <button onClick={() => signInWithEmailAndPassword(email, password)} type="submit" className="btn btn-info w-100">Login</button>
+                        <button type="submit" className="btn btn-info w-100 fw-bold">Login</button>
                     </form>
+                    <div className='text-dark text-end fw-bolder mt-2'>
+                        <p> New Here? <Link to='/' className='text-success text-decoration-none fw-bold'>Forgot Password </Link></p>
+                    </div>
                 </div>
             </div>
 
--- src/Pages/Auth/RequireAuth.js
+++ src/Pages/Auth/RequireAuth.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const RequireAuth = () => {
+const RequireAuth = ({children}) => {
     return (
         <div>
             <h2 className='display-5 text-center'>Check Out Page</h2>
