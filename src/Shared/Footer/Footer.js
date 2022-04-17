7920856236343792c6767c1b4037352ebe4436a5
hasanmint73 <hasanmint73@gmail.com>
2022-04-17 13:00:24 +0600

    Added Header and Footer

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Footer = () => {
     return (
         <div>
-            <h2 className='display-4 text-center'>Footer Page</h2>
+            <Card className='bg-light text-center variant-light'>
+                <Card.Body>Copyright@2022. All Right Reserved.2022</Card.Body>
+            </Card>
         </div>
     );
 };
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='display-4 text-center'>Header Page</h2>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">Logo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="#pricing">Services</Nav.Link>
+                        <Nav.Link href="#pricing">Blogs</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">About</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Login
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
