cd8aed94f4536c4fed0f6163937b406faac5d774
hasanmint73 <hasanmint73@gmail.com>
2022-04-18 15:16:26 +0600

    update 404 page

--- src/Shared/NotFound/NotFound.js
+++ src/Shared/NotFound/NotFound.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import notfound from '../../images/404page.png';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2 className='display-4 text-center'>Not Found Page</h2>
+        <div className='container'>
+            <div className="row my-5">
+                <div className="col-md-6 text-center mt-200">
+                    <h2 className="text-center">Error 404</h2>
+                    <h4 className="text-center"> Oops! The page you're looking for isn't here.</h4>
+                    <Link to="/" >
+                        <p className='btn btn-info text-light fw-bold'>Back to Home</p>
+                    </Link>
+
+                </div>
+                <div className="col-md-6">
+                    <img src={notfound} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
